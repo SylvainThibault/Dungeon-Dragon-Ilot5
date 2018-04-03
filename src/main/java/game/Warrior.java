4b3f316f2d4e96333116cb694d8e53a6a0c41a9c
Ana-Maria Olmo <ana-maria.olmo@campus-numerique-in-the-alps.com>
2018-04-03 11:22:56 +0200

    class perso Warrior Wizard

---
+++ src/main/java/game/Warrior.java
@@ -0,0 +1,33 @@
+package main.java.game;
+
+public class Warrior extends Perso {
+    private String weapon;
+    private int shield;
+
+
+    //ACCESSEURS//
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    // MUTATEURS//
+
+    public void setWeapon(String pWeapon){
+        weapon = pWeapon;
+    }
+
+    public void setShield(int pShield){
+        shield = pShield;
+    }
+
+    // Méthode toString : Affiche les informations d'un objet
+    public String toString(){
+        return super.toString() + "\nWeapon : " + weapon + "\nShield : " + shield;
+    }
+
+}
---
+++ src/main/java/game/Wizard.java
@@ -0,0 +1,32 @@
+package main.java.game;
+
+public class Wizard extends Perso {
+    private String spell;
+    private int philter;
+
+
+    //ACCESSEURS//
+
+    public String getSpell() {
+        return spell;
+    }
+
+    public int getPhilter() {
+        return philter;
+    }
+
+    // MUTATEURS//
+
+    public void setSpell(String pSpell){
+        spell = pSpell;
+    }
+
+    public void setPhilter(int pPhilter){
+        philter = pPhilter;
+    }
+
+    // Méthode toString : Affiche les informations d'un objet
+    public String toString(){
+        return super.toString() + "\nSpell : " + spell + "\nPhilter : " + philter;
+    }
+
