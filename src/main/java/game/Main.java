642eaff797b4568a533afea5a182488f194e2ebf
Ana-Maria Olmo <ana-maria.olmo@campus-numerique-in-the-alps.com>
2018-04-03 15:46:05 +0200

    added character creation and name choice

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -7,14 +7,18 @@ import java.util.concurrent.ThreadLocalRandom;
  * The type Main.
  */
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+
     /**
      * The entry point of application.
      *
      * @param args the input arguments
      */
+
+
     public static void main(String[] args) {
         playDice();
-
+        createCharacter();
     }
 
     public static Integer rollDice() {
@@ -26,7 +30,6 @@ public class Main {
         char diceYesNo = 'y';
         System.out.println("Do you want to roll the dice ? (y/whatever)");
         try {
-            Scanner sc = new Scanner(System.in);
             diceYesNo = sc.nextLine().charAt(0);
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("!!! On a dit 'y' ou 'n' bouletto !!!\n");
@@ -36,8 +39,70 @@ public class Main {
             System.out.println(rollDice());
             playDice();
         } else {
-            return ;
+            return;
         }
 
     }
+
+    public static void createCharacter() {
+        String choiceCharacter;
+        System.out.println("Choose your character : \n1.Warrior or \n2.Wizard");
+        choiceCharacter = sc.nextLine();
+
+        if (choiceCharacter.equals("1")) {
+            System.out.println("You chose to create a warrior");
+            createWarrior();
+        }
+
+        if (choiceCharacter.equals("2")) {
+            System.out.println("You chose to create a wizard");
+            createWizard();
+        }
+    }
+
+
+    public static void createWarrior() {
+        //************** création du personnage*****************//
+        Warrior warrior1 = new Warrior();
+
+        //****************choix du nom************************************************//
+        System.out.println("Choose the name  : ");
+        String newName = sc.nextLine();
+        warrior1.setName(newName);
+
+        //Affiche le nom du personnage
+        System.out.println("***********************************************");
+        System.out.println("Your warrior " + warrior1.getName() + " has been created");
+        System.out.println("***********************************************");
+        System.out.println("Do you want to create another character  :\n1.Yes or \n2.No ");
+        String choiceCreate;
+        choiceCreate = sc.nextLine();
+        if (choiceCreate.equals("1")) {
+            createCharacter();
+        } else {
+            playDice();
+        }
+    }
+
+
+    public static void createWizard() {
+
+        Wizard wizard1 = new Wizard();
+        System.out.println("Choose the name  : ");
+        String newName = sc.nextLine();
+        wizard1.setName(newName);
+        //Affiche le nom du personnage créé avec le message "personnage créé"
+        System.out.println("***********************************************");
+        System.out.println("Your wizard " + wizard1.getName() + " has been created");
+        System.out.println("***********************************************");
+        System.out.println("Do you want to create another character  :\n1.Yes or \n2.No ");
+        String choiceCreate;
+        choiceCreate = sc.nextLine();
+        if (choiceCreate.equals("1")) {
+            createCharacter();
+        } else {
+            playDice();
+        }
+    }
 }
+
