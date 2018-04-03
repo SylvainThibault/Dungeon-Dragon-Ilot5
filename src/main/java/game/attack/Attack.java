b09a883701c9282190543ea79a7582dfd6996f27
geraud-vercasson <geraud.vercasson@gmail.com>
2018-04-03 11:25:25 +0200

    attack name

--- src/main/java/game/attack/Attack.java
+++ src/main/java/game/attack/Attack.java
@@ -2,7 +2,27 @@ package game.attack;
 
 public abstract class Attack {
 
+    private String name;
     private int level;
+
+    /**
+     * Name Getter
+     * @return Name of the weapon / spell
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Name Setter
+     * @param name name of the weapon
+     */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     /**
      * Getter of the level attribute
