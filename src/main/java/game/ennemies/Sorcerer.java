09a1617489b40a5c2094ec79331edf7a2d409fff
Baptiste Jacquin <baptiste.jacquin@Digital-Grenoble.local>
2018-04-05 13:17:06 +0200

    commit pour le pull

--- src/main/java/game/ennemies/Sorcerer.java
+++ src/main/java/game/ennemies/Sorcerer.java
@@ -13,7 +13,7 @@ public class Sorcerer extends Enemy {
     public void fight(Perso perso){
         if( perso instanceof Wizard){
             int persoPower = perso.getPower();
-            int spellLevel=((Wizard) perso).getSpell().getLevel();
+            int spellLevel=((Wizard) perso).getSpell().getLevel(this);
             int degat = persoPower+spellLevel;
             int lifeEnemy =this.getLife();
             System.out.println("puissance d'attaque "+degat);
@@ -22,7 +22,14 @@ public class Sorcerer extends Enemy {
             if (lifeEnemy<=0){
                 System.out.println("you win");
             }else {
+                int persoLife = perso.getLife();
+                int attackEnemy = this.getAttack();
+                int changePersoLife = persoLife - attackEnemy;
                 System.out.println("you lose");
+                System.out.println(" life personnage "+persoLife);
+                perso.setLife(changePersoLife);
+                System.out.println("new life personnage "+persoLife);
+
             }
         }
     }
